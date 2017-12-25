a2fe4b121eab34fa3a38a15341ec6386f9138bdf
SWGawain <swgawain3120@gmail.com>
2017-12-25 22:47:27 +0800

    提交代码

--- run.py
+++ run.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 
-import requests ,re
+import requests ,re , random , time
 from ziroom import zrConfig
 
-hea = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36'}
+while True:
+    hea = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36'}
 
-page = requests.get("http://www.ziroom.com/z/vr/116152.html" , headers=hea)
-page.encoding = "utf-8"
+    page = requests.get("http://www.ziroom.com/z/vr/"+ str(zrConfig.room) +".html" , headers=hea)
+    page.encoding = "utf-8"
 
-photo = re.findall("http://pic.ziroom.com/static/images/slist_1207/defaultPZZ/natie-.+\.jpg" , page.text , re.S)
-print(photo)
+    loading = re.search('loading.jpg' , page.text , re.S)
+    canbook = re.search("canbook.jpg" , page.text , re.S)
 
+    if canbook or (not loading):
+        print("房源状态变更，可以预定")
+        zrConfig.sendNotice()
 
+    print(loading , canbook)
 
+    time.sleep(3 + random.random())
--- zrConfig.py
+++ zrConfig.py
@@ -3,15 +3,21 @@ from email.mime.text import MIMEText
 from email.header import Header
 import smtplib
 
-from_addr = "395772584@qq.com"
-password = "Wain920213"
-to_addr = ["395772584@qq.com","wangjiawei@facebank.cn"]
+# 发送邮件的邮箱
+from_addr = ""
+# 发送邮件的密码
+password = ""
+# 发送给谁 ，用','分割
+to_addr = [""]
 smtp_server = "smtp.qq.com"
+# 房源名称
+room_name = "茉莉园12号楼1101"
+# ziroom页面上房源编号
 room = 116152
 
-msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+msg = MIMEText(room_name+"可以预定了", 'plain', 'utf-8')
 msg['From'] = from_addr
-msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
+msg['Subject'] = Header('ziroom房源状态变更……', 'utf-8').encode()
 msg['To'] = ','.join(to_addr)
 
 def sendNotice():
